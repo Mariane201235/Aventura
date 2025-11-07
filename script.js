4b73a52799f3a86b8db99a6abff170d9899edc96
Mariane201235 <mariane.e.silva@escola.pr.gov.br>
2025-11-07 10:50:27 -0300

    hggf

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const avanca = document.querySelectorAll(". btn-proximo");
 avanca.porEach(button => {
     button.addEventListener('click, function(){
         const atual=document.querySelector('.ativo);
-        const proximoPasso = 'passo-
+        const proximoPasso = 'passo-' + this.getAttribute('data-proxima');
+
+        atual.classList.remove('ativo');
+        document.getElemen
         })
  })
